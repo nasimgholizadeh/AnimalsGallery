5d946eede65d6d7d58d5525c8d3ad69a3f4e06d1
nasim <nasim.2000ngh@gmail.com>
2020-06-23 21:38:30 +0430

    functions for get images

--- app/src/main/java/com/example/testapp/MainActivity.java
+++ app/src/main/java/com/example/testapp/MainActivity.java
@@ -3,8 +3,11 @@ package com.example.testapp;
 import android.Manifest;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
+import android.graphics.Color;
 import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ProgressBar;
 import android.widget.TextView;
@@ -13,7 +16,18 @@ import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import com.android.volley.DefaultRetryPolicy;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+import com.bumptech.glide.Glide;
 import com.zolad.zoominimageview.ZoomInImageView;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -74,5 +88,102 @@ public class MainActivity extends AppCompatActivity {
         else {
             return true;
         }
+    }
+
+    //function for get fox images
+    public void getFoxImage() {
+        //json request
+        JsonObjectRequest request=new JsonObjectRequest(Request.Method.GET,
+                "https://randomfox.ca/floof/?ref=public-apis", null, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                Log.i(TAG, "onResponse: "+response.toString());
+                try {
+                    image=response.getString("image"); //get json object
+                    /*put json object in imageView with Glide library
+                    glide library supports gif*/
+                    Glide.with(imageView).load(image).into(imageView);
+                    progressBar.setVisibility(View.INVISIBLE); //hide progress bar
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.e(TAG, "onErrorResponse: "+error.toString());
+                txtChooseAnimal.setText("لطفا از اتصال دستگاه خود به اینترنت مطمئن شوید.");
+                progressBar.setVisibility(View.GONE);
+                txtChooseAnimal.setTextColor(Color.RED);
+                btnSave.setEnabled(false);
+                btnFavourite.setEnabled(false);
+            }
+        });
+        request.setRetryPolicy(new DefaultRetryPolicy(8000,DefaultRetryPolicy.DEFAULT_MAX_RETRIES,
+                DefaultRetryPolicy.DEFAULT_BACKOFF_MULT));
+        RequestQueue requestQueue= Volley.newRequestQueue(this);
+        requestQueue.add(request);
+    }
+    //function for get dog images
+    private void getDogImage() {
+        JsonObjectRequest request=new JsonObjectRequest(Request.Method.GET,
+                "https://random.dog/woof.json", null, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                Log.i(TAG, "onResponse: "+response.toString());
+                try {
+                    image=response.getString("url");
+                    Glide.with(imageView).load(image).into(imageView); //put json object in imageView with Glide library
+                    progressBar.setVisibility(View.INVISIBLE); //hide progress bar
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.e(TAG, "onErrorResponse: "+error.toString());
+                txtChooseAnimal.setText("لطفا از اتصال دستگاه خود به اینترنت مطمئن شوید.");
+                progressBar.setVisibility(View.GONE);
+                txtChooseAnimal.setTextColor(Color.RED);
+                btnSave.setEnabled(false);
+                btnFavourite.setEnabled(false);
+            }
+        });
+        request.setRetryPolicy(new DefaultRetryPolicy(8000,DefaultRetryPolicy.DEFAULT_MAX_RETRIES,
+                DefaultRetryPolicy.DEFAULT_BACKOFF_MULT));
+        RequestQueue requestQueue= Volley.newRequestQueue(this);
+        requestQueue.add(request);
+    }
+    //function for get cat images
+    private void getCatImage() {
+        JsonObjectRequest request=new JsonObjectRequest(Request.Method.GET,
+                "https://aws.random.cat/meow?ref=public-apis", null, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                try {
+                    image=response.getString("file");
+                    Glide.with(imageView).load(image).into(imageView); //put json object in imageView with Glide library
+                    progressBar.setVisibility(View.INVISIBLE); //hide progress bar
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+                Log.i(TAG, "onResponse: "+response.toString());
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.e(TAG, "onErrorResponse: "+error.toString());
+                txtChooseAnimal.setText("لطفا از اتصال دستگاه خود به اینترنت مطمئن شوید.");
+                progressBar.setVisibility(View.GONE);
+                txtChooseAnimal.setTextColor(Color.RED);
+                btnSave.setEnabled(false);
+                btnFavourite.setEnabled(false);
+            }
+        });
+        request.setRetryPolicy(new DefaultRetryPolicy(8000,DefaultRetryPolicy.DEFAULT_MAX_RETRIES,
+                DefaultRetryPolicy.DEFAULT_BACKOFF_MULT));
+        RequestQueue requestQueue= Volley.newRequestQueue(this);
+        requestQueue.add(request);
     }
 }
