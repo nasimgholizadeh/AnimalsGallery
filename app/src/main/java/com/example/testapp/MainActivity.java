d3e1fab06e3db4497c0baafb1ccb452176393af7
nasim <nasim.2000ngh@gmail.com>
2020-06-23 21:14:56 +0430

    define variables

--- app/src/main/java/com/example/testapp/MainActivity.java
+++ app/src/main/java/com/example/testapp/MainActivity.java
@@ -1,10 +1,34 @@
 package com.example.testapp;
 
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import com.zolad.zoominimageview.ZoomInImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+
+    private Button btnFox,btnDog,btnCat,btnSave,btnFavourite;
+    private ZoomInImageView imageView;
+    private TextView txtChooseAnimal;
+    private ProgressBar progressBar;
+
+    private Toolbar mToolbar;
+
+    private String image;
+
+    Bitmap bitmap;
+
+    private long backPressedTime;
+    private Toast backToast;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
