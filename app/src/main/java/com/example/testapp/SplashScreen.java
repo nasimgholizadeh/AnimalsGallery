18e3c49ef33612a08630156bddfd181c4928b5ed
Nasim Gholizadeh <nasim.2000ngh@gmail.com>
2020-07-01 10:34:40 +0430

    added splash screen

---
+++ app/src/main/java/com/example/testapp/SplashScreen.java
@@ -0,0 +1,27 @@
+package com.example.testapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashScreen extends AppCompatActivity {
+
+    private final int SPLASH_DISPLAY_LENGTH=1500;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent=new Intent(SplashScreen.this,MainActivity.class);
+                SplashScreen.this.startActivity(intent);
+                SplashScreen.this.finish();
+            }
+        },SPLASH_DISPLAY_LENGTH);
+    }
+}
